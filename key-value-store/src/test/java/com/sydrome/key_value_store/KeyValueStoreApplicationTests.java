0fdc4e465efac28933a7b63cc3c62a56efe8fa54
SahilsDevelopment <prof.se.sahil@gmail.com>
2025-09-13 21:31:03 +0530

    Added Spring Project

---
+++ key-value-store/src/test/java/com/sydrome/key_value_store/KeyValueStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sydrome.key_value_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyValueStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
